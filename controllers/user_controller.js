0ec63702b517032162cc8c39d59f17cc605008e1
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-04-19 20:58:04 +0530

    final

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,33 @@
 const User = require('../models/user')
 
-module.exports.profile = (req, res) => {
-    return res.render('user_profile', {
-        title: "Social App / Profile"
-    })
+module.exports.profile = async (req, res) => {
+    try {
+        if (req.cookies.user_id) {
+            const existingUser = await User.findById(req.cookies.user_id)
+            if (existingUser) {
+                return res.render('user_profile', {
+                    title: "Social App / Profile",
+                    user: existingUser
+                })
+            }
+            else {
+                return res.render('user_sign_in', {
+                    title: "Social App / Profile"
+                })
+            }
+        }
+        else {
+            return res.render('user_sign_in', {
+                title: "Social App / Profile"
+            })
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.render('user_sign_in', {
+            title: "Social App / Profile"
+        })
+    }
 }
 
 module.exports.signup = (req, res) => {
@@ -12,10 +36,11 @@ module.exports.signup = (req, res) => {
     })
 }
 
-module.exports.signin = (req, res) => {
+module.exports.signin = async (req, res) => {
     return res.render('user_sign_in', {
         title: "Social App/ Signin"
     })
+
 }
 
 module.exports.create = async (req, res) => {
@@ -35,6 +60,25 @@ module.exports.create = async (req, res) => {
     }
 }
 
-module.exports.createSession = (req, res) => {
+module.exports.createSession = async (req, res) => {
+    try {
+        // console.log('inside create session',req.body.email);
+        const existingUser = await User.findOne({ email: req.body.email })
+        // console.log(existingUser);
+        if (!existingUser || existingUser.password != req.body.password) {
+            return res.render('user_sign_in', {
+                title: "Social App/ Signin"
+            })
+        }
+        else {
+            console.log('login success')
+            res.cookie('user_id', existingUser.id)
+            return res.redirect('/user/profile')
+        }
 
+    }
+    catch (err) {
+        console.log('login failed')
+        return res.redirect('/user/profile')
+    }
 }
