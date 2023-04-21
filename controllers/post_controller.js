30ce1fbc6d06951994c9f324501ec3ecca0a41e8
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-04-21 19:51:14 +0530

    delete a post implemented

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,15 +1,37 @@
-const Post=require('../models/post')
+const Post = require('../models/post')
+const Comment = require('../models/comment')
+module.exports.create = async (req, res) => {
+    try {
+        const createdPost = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        })
+        return res.redirect('/')
+    }
+    catch (err) {
+        console.log(err);
+        return res.redirect('/')
+    }
+}
 
-module.exports.create=async(req,res)=>{
-    try{
-    const createdPost = await Post.create({
-        content: req.body.content,
-        user: req.user._id
-    })
-    return res.redirect('/')
-}
-catch(err){
-    console.log(err);
-    return res.redirect('/')
-}
+module.exports.destroy = async (req, res) => {
+    try {
+        const existingPost = await Post.findById(req.params.id)
+        if (existingPost) {
+            
+            if (existingPost.user == req.user.id) { 
+                await Comment.deleteMany({ post: existingPost.id })
+                await Post.deleteOne({ _id: existingPost.id })
+            }
+            else { console.log(existingPost,req.user)
+                console.log('post id not matched'); }
+
+        }
+        else { console.log('post not found'); }
+        return res.redirect('/')
+    }
+    catch (err) {
+        console.log(err);
+        return res.redirect('/')
+    }
 }
