af98fde0b4c7c1171398af5a495f24684528237b
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-04-21 21:30:44 +0530

    comment deletion with auth implemented

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -5,17 +5,35 @@ module.exports.create = async (req, res) => {
     try {
         const existingPost = await Post.findById(req.body.post)
         if (existingPost) {
-            const newComment=await Comment.create({
+            const newComment = await Comment.create({
                 content: req.body.content,
                 user: req.user._id,
                 post: req.body.post
             })
-        existingPost.comment.push(newComment)
-        existingPost.save()
+            existingPost.comment.push(newComment)
+            existingPost.save()
         }
         res.redirect('/')
     }
-    catch(err){
+    catch (err) {
         console.log(err);
+    }
+}
+
+module.exports.destroy = async (req, res) => {
+    try {
+        const existingComment = await Comment.findById(req.params.id)
+        // console.log(existingComment.user._id.toString() , req.user.id);
+        if (existingComment.user._id.toString() == req.user.id) {
+            const existingPost = await Post.findById(existingComment.post)
+            Post.findByIdAndUpdate(existingComment.post, { $pull: { comment: req.params.id } })
+            await Comment.deleteOne({ _id: existingComment.id })
+        }
+        return res.redirect('/')
+    }
+    catch (err) {
+        console.log(err);
+        return res.redirect('/')
+
     }
 }
