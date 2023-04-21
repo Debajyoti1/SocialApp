dc4d07969fc69c73c82a9f84fe56910d0dda6e53
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-04-21 11:49:49 +0530

    populated user from id to complete user,rendered

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,20 @@
-module.exports.home=(req,res)=>{
+const Post=require('../models/post')
+
+module.exports.home=async (req,res)=>{
     // console.log(req.cookies);
     // res.cookie('b','c')
-    
+    try{
+    const posts=await Post.find({}).populate('user')
     return res.render('home',{
-        title: "Home"
+        title: "Home",
+        posts: posts
+    })
+    }
+    catch(err){
+        console.log(err);
+    }
+    return res.render('home',{
+        title: "Home",
+        posts: {}
     })
 }
