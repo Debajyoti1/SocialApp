2f4b0d70af448c0ed5da366901c68889be65b76a
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-04-22 17:33:33 +0530

    post added using AJAX

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -1,25 +1,45 @@
 {
     //post request to create a post
     console.log('hello');
-    let createtPost=()=>{
-        let newPostForm=$('#new-post-form')
-        newPostForm.submit((e)=>{
+    let createtPost = () => {
+        let newPostForm = $('#new-post-form')
+        newPostForm.submit((e) => {
             e.preventDefault()
 
             $.ajax({
                 type: 'post',
                 url: '/post/create',
                 data: newPostForm.serialize(),
-                success: (data)=>{
+                success: (data) => {
                     console.log(data);
+                    let newPost=newPostDom(data.data.post)
+                    $('.post-list>ul').prepend(newPost)
                 },
-                error: (error)=>{
+                error: (error) => {
                     console.log(error.resposeText);
                 }
             })
         })
     }
+    //method to create the post in DOM
+    let newPostDom = (post) => {
+        return $(`
+            <li id="post-id-${post._id}">
+            <p>${post.content}
+                <small>${post.user.name}</small>
+                <small><a class="delete-post-button" href="/post/destroy/${post.id}">Delete</a></small>
+            </p>
+                <div class="post-comment-add">
+                    <form action="/comment/create" method="post">
+                        <input type="text" name="content" placeholder="Type Here to comment..." required>
+                        <input type="hidden" name="post" value="${post._id}">
+                        <button type="submit">Comment</button>
+                    </form>
+                </div>
+            </li>
+        `)
+    }
+
     createtPost()
 
-    //method to create the post in DOM
 }
